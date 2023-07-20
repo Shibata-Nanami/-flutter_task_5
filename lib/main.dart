ee4f9f68f9c81c2d0d1b3d852fa2e7a25db4279e
Shibata-Nanami <n.shibata@visionary-jp.net>
2023-07-20 13:34:00 +0900

    firebase設定　エラー修正

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'firebase_options.dart';
 import 'memo.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
